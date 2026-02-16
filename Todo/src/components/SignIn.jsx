277d088ceda15431bb87863c41a6d6fae6b6cd1d
Mayank <mk20040307@gmail.com>
2026-02-16 21:33:04 +0530

    change

--- Todo/src/components/SignIn.jsx
+++ Todo/src/components/SignIn.jsx
@@ -1,6 +1,6 @@
 import React, {useState} from 'react'
 import { Link, useNavigate } from 'react-router-dom'
-import api from '../Api'
+import api from '../api'
 
 const SignIn = () => {
     const [email, setEmail] = useState("")
