76565e034decc7de54cbcd4571096031e1b3fc98
Mayank <mk20040307@gmail.com>
2026-02-17 09:48:22 +0530

    changed background

--- Todo/src/components/Todo.jsx
+++ Todo/src/components/Todo.jsx
@@ -11,18 +11,10 @@ const Todo = () => {
      const { user } = useContext(AuthContext);
 
   
-  //   useEffect(() => {
-    
-  //   const storedTodos = JSON.parse(localStorage.getItem("todos"));
-  //   if(storedTodos && storedTodos.length>0) setTodos(storedTodos)
-  // }, [])
   
-  // useEffect(() => {
-  //   localStorage.setItem("todos", JSON.stringify(todos))
-  // }, [todos])
   
   return (
-    <div className="bg-[#172842] min-h-screen py-8">
+    <div className="bg-[#658953] min-h-screen py-8">
                 <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
                     <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
                     <div className="mb-4">
