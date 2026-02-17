481c9216c4172ea0cfc7e98516dfe60992aed231
Mayank <mk20040307@gmail.com>
2026-02-17 09:54:40 +0530

    changed background

--- Todo/src/components/Home.jsx
+++ Todo/src/components/Home.jsx
@@ -5,11 +5,11 @@ const Home = () => {
   return (
     <section className="flex flex-col items-center text-center mt-20 px-4">
         <h2 className="text-4xl font-bold mb-4">
-          Secure Your Passwords With Ease 🔐
+          Secure Your Todo With Ease 🔐
         </h2>
 
         <p className="text-gray-600 max-w-xl mb-8">
-          Store, manage and protect your passwords in one secure place.
+          Store, manage and protect your todos in one secure place.
           Simple, fast and encrypted.
         </p>
 
@@ -20,11 +20,7 @@ const Home = () => {
           </button>
           </Link>
  
-          {/* <Link to="features">
-          <button className="px-6 py-3 border rounded-lg hover:bg-gray-100">
-            Explore
-          </button>
-          </Link> */}
+          
           
         </div>
         <section className="mt-20 max-w-6xl mx-auto px-6 grid md:grid-cols-3 gap-8">
